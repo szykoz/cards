3b331cb125cd72938157f14a9c7d3532abde832d
szykoz <szykoz@outlook.com>
2025-05-29 22:47:12 +0200

    initial commit

---
+++ src/test/java/com/skoziol/cards/CardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skoziol.cards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
